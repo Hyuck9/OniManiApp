d16f68943df97d4addfd46b0ba24b02a629c7881
Hyuck9 <lhg1304@gmail.com>
2018-07-16 13:34:23 +0900

    initialize MainActivity, FriendsFragment, AppointFragment

--- app/src/main/java/kr/co/nexmore/onimaniapp/views/LoginActivity.kt
+++ app/src/main/java/kr/co/nexmore/onimaniapp/views/LoginActivity.kt
@@ -2,9 +2,10 @@ package kr.co.nexmore.onimaniapp.views
 
 import android.content.Intent
 import android.graphics.Color
-import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.remoteconfig.FirebaseRemoteConfig
 import kotlinx.android.synthetic.main.activity_login.*
 import kr.co.nexmore.onimaniapp.R
@@ -12,21 +13,63 @@ import kr.co.nexmore.onimaniapp.R
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var mFirebaseRemoteConfig: FirebaseRemoteConfig
+    private lateinit var mFirebaseAuth: FirebaseAuth
+    private lateinit var mAuthStateListener: FirebaseAuth.AuthStateListener
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
         mFirebaseRemoteConfig = FirebaseRemoteConfig.getInstance()
+        mFirebaseAuth = FirebaseAuth.getInstance()
+        mFirebaseAuth.signOut()
+
         val splashBackground = mFirebaseRemoteConfig.getString(getString(R.string.rc_color))
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            window.statusBarColor = Color.parseColor(splashBackground)
+        window.statusBarColor = Color.parseColor(splashBackground)
+
+        // 로그인 인터페이스 리스너
+        mAuthStateListener = FirebaseAuth.AuthStateListener {
+            // mFirebaseAuth.currentUser != null 작업 수행 (Safe calls + let 사용)
+            mFirebaseAuth.currentUser?.let {
+                startActivity(Intent(this, MainActivity::class.java))
+                finish()
+            }
         }
 
-        login_btn_login.setBackgroundColor(Color.parseColor(splashBackground))
-        login_btn_signup.setBackgroundColor(Color.parseColor(splashBackground))
+        login_a_btn_login.setBackgroundColor(Color.parseColor(splashBackground))
+        login_a_btn_signup.setBackgroundColor(Color.parseColor(splashBackground))
 
-        login_btn_signup.setOnClickListener {
+        login_a_btn_login.setOnClickListener {
+            loginEvent()
+        }
+        login_a_btn_signup.setOnClickListener {
             startActivity(Intent(this, SignupActivity::class.java))
         }
     }
+
+    private fun loginEvent() {
+        val id = login_a_et_id.text.toString()
+        val pw = login_a_et_password.text.toString()
+
+        if ( id.isEmpty() || pw.isEmpty() ) {
+            Toast.makeText(this, "아이디와 비밀번호를 입력해 주세요.", Toast.LENGTH_LONG).show()
+            return
+        }
+        mFirebaseAuth.signInWithEmailAndPassword(id, pw).addOnCompleteListener {
+            if (!it.isSuccessful) {
+                // 로그인 실패
+                Toast.makeText(this, it.exception!!.message , Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mFirebaseAuth.addAuthStateListener(mAuthStateListener)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mFirebaseAuth.removeAuthStateListener(mAuthStateListener)
+    }
+
 }
--- app/src/main/java/kr/co/nexmore/onimaniapp/views/MainActivity.kt
+++ app/src/main/java/kr/co/nexmore/onimaniapp/views/MainActivity.kt
@@ -2,21 +2,62 @@ package kr.co.nexmore.onimaniapp.views
 
 import android.os.Bundle
 import android.support.design.widget.Snackbar
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentPagerAdapter
 import android.support.v7.app.AppCompatActivity
 import kr.co.nexmore.onimaniapp.R
 
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
+import java.util.ArrayList
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mPagerAdapter : ViewPagerAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
+        setSupportActionBar(main_a_toolbar)
+        main_a_toolbar.setLogo(R.drawable.loading_icon)
+        main_a_tabs.setupWithViewPager(main_c_viewpager)
+        this.setUpViewPager()
 
         main_a_fab_main.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
+        }
+    }
+
+    private fun setUpViewPager() {
+        mPagerAdapter = ViewPagerAdapter(supportFragmentManager)
+        mPagerAdapter.addFragment(AppointFragment(), "약속")
+        mPagerAdapter.addFragment(FriendsFragment(), "친구")
+        main_c_viewpager.adapter = mPagerAdapter
+    }
+
+
+    private inner class ViewPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
+
+        private val fragmentList = ArrayList<Fragment>()
+        private val fragmentTitleList = ArrayList<String>()
+
+        override fun getItem(position: Int): Fragment {
+            return fragmentList[position]
+        }
+
+        override fun getPageTitle(position: Int): CharSequence? {
+            return fragmentTitleList[position]
+        }
+
+        override fun getCount(): Int {
+            return fragmentList.size
+        }
+
+        fun addFragment(fragment: Fragment, title: String) {
+            fragmentList.add(fragment)
+            fragmentTitleList.add(title)
         }
     }
 
